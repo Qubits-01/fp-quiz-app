6ea3a8fb5c256d37918eae0cbb47a12684df59a7
Marc Peejay Viernes <mpvviernes@gmail.com>
2021-02-03 15:37:24 +0800

    Add app logic and widgets

---
+++ lib/question.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String questionText;
+
+  Question(this.questionText);
+
+  /*
+  Question(question) {
+    this.questionText = question;
+  }
+  */
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
